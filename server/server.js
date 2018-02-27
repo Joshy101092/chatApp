fdf733a4e09cfe6308d0095398aa41a91c9e6098
Joshua Fidler-Brown <joshuafidlerbrown@hotmail.com>
2018-02-27 22:55:05 +0000

    modified:   package.json 	modified:   public/index.html 	new file:   public/js/index.js 	modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,33 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname,"../public")
-
-
 const app = express();
 var port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on("connection", (socket) =>{
+    console.log("New user connected");
+
+    socket.on("createMessage",(newMessage) =>{
+        console.log("Message received", newMessage);
+        io.emit("newMessage", {
+            from:newMessage.from,
+            text: newMessage.text,
+            createdAt: new Date().getTime()
+        })
+    })
+
+socket.on("disconnect", () =>{
+    console.log("User disconnected");
+    })
+
+})
 app.get("/",(req, res) => {
 
     //res.send("<h1>Hello Express!</h1>");
@@ -17,6 +36,6 @@ app.get("/",(req, res) => {
 
 
 
-app.listen(port,() => {
+server.listen(port,() => {
     console.log(`Server running on port: ${port}`)
 })
